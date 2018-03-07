31de97a82d11888b91330c7659ea1bef2f06ec06
Anas Khan <anas.khan96@outlook.com>
2018-03-07 22:10:47 +0530

    making the symbol table class

--- src/symbol_table.py
+++ src/symbol_table.py
@@ -1,2 +1,14 @@
-# Symbol table
-st = []
+class symbol_table:
+	def __init__(self, name, parent):
+		self.symbols = {}
+		self.name = name
+		self.parent = parent
+
+	def insert(self, symbol, token):
+		self.symbols[symbol] = token	
+
+	def lookup(self, symbol):
+		return self.symbols[symbol]
+
+	def getParent(self):
+		return self.parent
